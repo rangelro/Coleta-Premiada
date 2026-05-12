e0dc56c6efe3e44c32019387daeb3f4e57f2b093
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2026-05-12 20:02:21 -0300

    chore: register de imovel e saldoPontos no painel de admin

--- services/core/program/admin.py
+++ services/core/program/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Imovel, SaldoPontos
 
-# Register your models here.
+
+@admin.register(Imovel)
+class ImovelAdmin(admin.ModelAdmin):
+    list_display = ('inscricao', 'nome_titular', 'cpf_titular', 'num_moradores', 'ativo', 'data_adesao')
+    search_fields = ('inscricao', 'nome_titular', 'cpf_titular')
+    list_filter = ('ativo', 'data_adesao')
+
+@admin.register(SaldoPontos)
+class SaldoPontosAdmin(admin.ModelAdmin):
+    list_display = ('imovel', 'ciclo', 'desconto_percentual', 'atualizado')
+    search_fields = ('imovel__inscricao', 'imovel__nome_titular')
+    list_filter = ('ciclo',)
