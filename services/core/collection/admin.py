e1f9cd1f55b78fab7cb079ae2a98a1e60fa022a9
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2026-05-12 20:03:02 -0300

    chore: register de registro de coleta  no painel de admin

--- services/core/collection/admin.py
+++ services/core/collection/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import RegistroColeta
 
-# Register your models here.
+
+@admin.register(RegistroColeta)
+class RegistroColetaAdmin(admin.ModelAdmin):
+    list_display = ('id_microservico', 'inscricao_imobiliaria', 'imovel', 'pontuacao', 'data_hora_coleta')
+    search_fields = ('id_microservico', 'inscricao_imobiliaria')
+    list_filter = ('data_hora_coleta',)
