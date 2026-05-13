87a2f9e674752e60595a1240a64dd858d94139fa
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2026-05-13 17:46:33 -0300

    fix: corrigindo campos de admin para não ter comnflitos com os campos retirados da class RegistroColetaAdmin

--- services/core/collection/admin.py
+++ services/core/collection/admin.py
@@ -4,6 +4,6 @@ from .models import RegistroColeta
 
 @admin.register(RegistroColeta)
 class RegistroColetaAdmin(admin.ModelAdmin):
-    list_display = ('id_microservico', 'inscricao_imobiliaria', 'imovel', 'pontuacao', 'data_hora_coleta')
-    search_fields = ('id_microservico', 'inscricao_imobiliaria')
-    list_filter = ('data_hora_coleta',)
+    list_display = ('id_microservico', 'imovel', 'pontuacao')
+    search_fields = ('id_microservico',)
+    # list_filter = ('data_hora_coleta',)
