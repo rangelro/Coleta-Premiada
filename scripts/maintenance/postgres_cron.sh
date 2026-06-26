690f5ba309e7b3d7513b8e6284f5a8ef56c3f31f
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2026-06-25 21:56:51 -0300

    fix: movendo do docker compose para postgres_cron.sh

---
+++ scripts/maintenance/postgres_cron.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -eu
+
+cat > /etc/crontabs/root <<EOF
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+PGPASSWORD=${PGPASSWORD}
+PGHOST=${PGHOST:-core-db}
+PGPORT=${PGPORT:-5432}
+PGUSER=${PGUSER}
+PGDATABASE=${PGDATABASE}
+LOG_RETENTION_DAYS=${LOG_RETENTION_DAYS:-90}
+LOG_CLEANUP_BATCH_SIZE=${LOG_CLEANUP_BATCH_SIZE:-5000}
+
+15 2 * * * sh /maintenance/cleanup_logs.sh
+45 2 * * * sh /maintenance/vacuum_analyze.sh
+30 3 * * 0 sh /maintenance/reindex.sh
+EOF
+
+echo "[$(date -Iseconds)] PostgreSQL maintenance cron configured"
+exec crond -f -l 8
